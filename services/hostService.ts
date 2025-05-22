b1cd6f5964e614f6ef0af7022bf1657e473fcff2
Lorenzo Coslado <lorenzo.coslado@gmail.com>
2025-05-22 15:25:04 +0200

    VMFenelon

--- services/hostService.ts
+++ services/hostService.ts
@@ -1,5 +1,5 @@
 const hostService = {
-  API_URL: "http://104.248.139.190:3001/api",
+  API_URL: "http://104.248.139.190:3001/api", // http://172.16.193.254:3001/api pour la VM Fenelon
 };
 
 export default hostService;
